0a3004e5c8c9d5175002aae17ac0292474486934
Viktor Karlsson <viktor.karlsson2@elev.ga.ntig.se>
2022-08-30 10:53:00 +0200

    initial commit

---
+++ Lucktext_L_plus_Ratio/Program.cs
@@ -0,0 +1,22 @@
+﻿string name; 
+string cold;
+string idk;
+name = "one"; 
+cold = "two";
+idk = "three";
+Console.WriteLine("Whats your name?");
+name = Console.ReadLine();
+
+Console.WriteLine($"what's your favorite hobby {name}?");
+cold = Console.ReadLine();
+
+Console.WriteLine($"Oh really, {cold}? That's kinda sad cause you are really bad at it");
+idk = Console.ReadLine(); 
+
+Console.WriteLine("Aight stop getting so mad. Imma let you be done now, just press ENTER to leave");
+Console.ReadLine();
+
+
+
+
+
